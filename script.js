9a685d6db90c2ed81f5601f3c3219be0c6f10ba7
Caio Secco <caio.secco@hotmail.com>
2026-07-24 17:12:53 -0300

    update code

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 const menuToggle = document.getElementById("menuToggle");
 const nav = document.getElementById("nav");
 
+
+// MENU MOBILE
 menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
+    nav.classList.toggle("active");
 });
+
+
+
+// ANIMAÇÃO AO SCROLL
+
+const reveals = document.querySelectorAll(".reveal");
+
+
+function revealOnScroll() {
+
+    reveals.forEach((element) => {
+
+        const windowHeight = window.innerHeight;
+
+        const elementTop = element.getBoundingClientRect().top;
+
+        const revealPoint = 120;
+
+
+        if (elementTop < windowHeight - revealPoint) {
+
+            element.classList.add("active");
+
+        }
+
+    });
+
+}
+
+
+window.addEventListener("scroll", revealOnScroll);
+
+
+// Executa ao abrir a página
+revealOnScroll();
+
+
+
+// ANO AUTOMÁTICO FOOTER
+
+const year = document.getElementById("year");
+
+if(year){
+
+    year.textContent = new Date().getFullYear();
+
+}
